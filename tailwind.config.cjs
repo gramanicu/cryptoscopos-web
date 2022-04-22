a84b256c3cc6a6e48f32a42e8f312d7581528650
Grama Nicolae <gramanicu@gmail.com>
2022-04-22 17:51:21 +0300

    Created skeleton

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: ['./src/**/*.html', './src/**/*.svelte'],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+};
