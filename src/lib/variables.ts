b7dd1062f10cc3bd2147bd24ca540d1fee338046
Grama Nicolae <gramanicu@gmail.com>
2022-04-25 06:36:08 +0300

    Added auth to api

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,4 +1,10 @@
 export const vars = {
-	domain: import.meta.env.VITE_DOMAIN,
-	clientId: import.meta.env.VITE_CLIENT_ID
+	auth0: {
+		domain: import.meta.env.VITE_AUTH0_DOMAIN,
+		clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+		audience: import.meta.env.VITE_AUTH0_AUDIENCE
+	},
+	api: {
+		baseUrl: import.meta.env.VITE_API_BASE_URL
+	}
 };
